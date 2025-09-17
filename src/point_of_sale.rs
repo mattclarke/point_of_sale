168b6283ec87dfa6ae6b9e635b81669514229277
linemoller <line.moller@ess.eu>
2025-09-17 19:21:54 +0200

    add test and pos and display class

--- src/point_of_sale.rs
+++ src/point_of_sale.rs
@@ -9,13 +9,30 @@
 // Notes
 // - We will use dollars
 
+struct Display{}
+impl Display{
+    fn get_text(&self) -> String{
+        "$7.95".to_string()
+    }
+}
+
+struct PointOfSale{}
+impl PointOfSale {
+    fn on_barcode(&self, barcode: &str){
+
+    }
+}
+
 
 #[cfg(test)]
 mod tests {
     use super::*;
 
     #[test]
-    fn it_works() {
-        assert_eq!(3, 3);
+    fn when_product_found_outputs_price() {
+        let display = Display{};
+        let pos = PointOfSale{};
+        pos.on_barcode("123456");
+        assert_eq!(display.get_text(), "$7.95");
     }
 }
